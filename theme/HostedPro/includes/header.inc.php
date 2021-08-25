288f9a3b7630283daf999564ce8d3ddae47e3af3
John Stray <johnstray@users.noreply.github.com>
2021-08-25 17:10:57 +0930

    Update header.inc.php

--- theme/HostedPro/includes/header.inc.php
+++ theme/HostedPro/includes/header.inc.php
@@ -1,9 +1,9 @@
 <?php if ( defined('IN_GS') === false ) { die('You cannot load this file directly.'); }
 /***************************************************************************************************
 *
-* @File: 	    header.inc.php
-* @Package:		HostYa Website Theme
-* @Action:		HTML Header and Metadata
+* @File:        header.inc.php
+* @Package:     HostedPro Website Theme
+* @Action:      HTML Header and Metadata
 *
 ***************************************************************************************************/
 ?><!DOCTYPE html><!-- HTML5 : XHTML Compatible -->
@@ -23,12 +23,12 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
 	    
         <!-- Stylesheets -->
-	      <link rel="stylesheet" id="bootstrap"      href="<?php get_theme_url(); ?>/assets/css/bootstrap.min.css" />
-	      <link rel="stylesheet" id="fontawesome"    href="<?php get_theme_url(); ?>/assets/css/font-awesome.min.css" />
-	      <link rel="stylesheet" id="main-style"     href="<?php get_theme_url(); ?>/assets/css/main-styles.css" />
+        <link rel="stylesheet" id="bootstrap"      href="<?php get_theme_url(); ?>/assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" id="fontawesome"    href="<?php get_theme_url(); ?>/assets/css/font-awesome.min.css" />
+        <link rel="stylesheet" id="main-style"     href="<?php get_theme_url(); ?>/assets/css/main-styles.css" />
         <!-- @TODO: Implement more color options and get selected color from theme settings plugin -->
-	      <link rel="stylesheet" id="theme-color"    href="<?php get_theme_url(); ?>/assets/css/colors/blue.css" />
-	      <link rel="stylesheet" id="custom-styles"  href="<?php get_theme_url(); ?>/assets/css/custom.css" />
+        <link rel="stylesheet" id="theme-color"    href="<?php get_theme_url(); ?>/assets/css/colors/blue.css" />
+        <link rel="stylesheet" id="custom-styles"  href="<?php get_theme_url(); ?>/assets/css/custom.css" />
       
         <!--
              @TODO: Add metadata/opengraph/twittercard support
@@ -37,13 +37,13 @@
       
         <?php get_header(); // GetSimple Header Hook - Used by many plugins to add functionality ?>
 	    
-	  </head>
+    </head>
 	
-	  <body id="<?php get_page_slug(); ?>">
+    <body id="<?php get_page_slug(); ?>">
 	    
-	      <div class="preloader">
-	          <div class="spinner">
-	              <div class="double-bounce1"></div>
-	              <div class="double-bounce2"></div>
-	          </div>
-	      </div>	    
+        <div class="preloader">
+            <div class="spinner">
+                <div class="double-bounce1"></div>
+                <div class="double-bounce2"></div>
+            </div>
+		</div>

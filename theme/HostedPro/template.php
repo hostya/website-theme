a1d4264ba615cd901659dee56716652cbe68027c
John Stray <johnstray@users.noreply.github.com>
2021-08-25 16:39:36 +0930

    Create template.php

---
+++ theme/HostedPro/template.php
@@ -0,0 +1,34 @@
+<?php if ( defined('IN_GS') === false ){ die('You cannot load this file directly.'); }
+/****************************************************
+*
+* @File: 	    template.php
+* @Package:		GetSimple CMS
+* @Action:		HostedPro theme for GetSimple CMS
+*
+*****************************************************/
+
+
+# Get this theme's settings based on what was entered within its plugin. 
+# This function is in functions.php 
+//$hostedpro_settings = HostedProTheme_Settings();
+
+# Include the header template
+include('includes/header.inc.php');
+
+# Include the primary navigation
+include('includes/navigation.inc.php');
+?>
+
+        <main id="main-content">
+
+            <!-- BREADCRUMBS - @TODO: Refacter this to use custom theme plugin -->
+            <?php if ( return_custom_field('hide-bc') != 'on' ) { include('includes/breadcrumbs.inc.php'); } ?>
+
+            <article>
+                <?php get_page_content(); ?>
+            </article>
+
+        </main>
+
+<?php # Include the footer template
+include('includes/footer.inc.php');
